e3da5ecfc3b11852bf64d8dce72cc480e2ac847a
Monal Mahajan <monal.mahajan@hotmail.com>
2020-01-28 20:59:25 -0500

    Flix Part 1

--- FlixMovies/MoviesViewController.swift
+++ FlixMovies/MoviesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -55,7 +56,12 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let synopsis = movie["overview"] as! String
         
         cell.titleLabel.text = title
-        cell.synopsisLabel.text = "synopsis"
+        cell.synopsisLabel.text = synopsis
+        let base_url = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: base_url + posterPath)!
+        
+        cell.posterView.af_setImage(withURL: posterUrl)
         
         return cell
     }
