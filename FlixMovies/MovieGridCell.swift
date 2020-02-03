f97cad919d7781a730ccb0155e2e3e5a099c2335
Monal Mahajan <monal.mahajan@hotmail.com>
2020-02-02 23:31:01 -0500

    Part 2

---
+++ FlixMovies/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  FlixMovies
+//
+//  Created by Monal on 2/2/20.
+//  Copyright © 2020 Monal. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
